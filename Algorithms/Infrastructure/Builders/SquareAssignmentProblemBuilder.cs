ca39e5a689785ce39b174ffd2b646832a5514508
ORKEMEY <p.artem.p0@ukr.net>
2021-05-27 01:02:12 +0300

    Result writer & new extensions added

--- Algorithms/Infrastructure/Builders/SquareAssignmentProblemBuilder.cs
+++ Algorithms/Infrastructure/Builders/SquareAssignmentProblemBuilder.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.Json;
 using System.IO;
 using System.Threading.Tasks;
+using Infrastructure.Extensions;
 
 namespace Infrastructure
 {
@@ -14,16 +15,45 @@ namespace Infrastructure
 		{
 			return new SquareAssignmentProblem(matrixC: matrixC, matrixT: matrixT);
 		}
-
+		/// <summary>
+		/// Reads task from file
+		/// </summary>
+		/// <param name="Path"></param>
+		/// <exception cref="FileNotFoundException"></exception>
+		/// <returns></returns>
 		public async override Task<SquareAssignmentProblem> CreateAsync(string Path)
 		{
 			AssignmentProblemDTO restoredProb;
+
+			if (!File.Exists(Path)) throw new FileNotFoundException("Not existing file specified", Path);
 			using (FileStream fs = new FileStream(Path, FileMode.OpenOrCreate))
 			{
 				restoredProb = await JsonSerializer.DeserializeAsync<AssignmentProblemDTO>(fs);
 			}
 
-			return new SquareAssignmentProblem(matrixC: restoredProb.GetMatrixC(), matrixT: restoredProb.GetMatrixT());
+			if (restoredProb.MatrixC == null || restoredProb.MatrixT == null) 
+				throw new FormatException("Wrong format of file (make sure properties are named correctly)");
+
+			int[,] matrixC, matrixT;
+			try
+			{
+				matrixC = restoredProb.MatrixC.ToTwoDimArray();
+			}
+			catch (ArgumentException ex) when (ex.ParamName == null)
+			{
+				throw new ArgumentException(ex.Message, nameof(restoredProb.MatrixC));
+			}
+			try
+			{
+				matrixT = restoredProb.MatrixT.ToTwoDimArray();
+			}
+			catch (ArgumentException ex) when (ex.ParamName == null)
+			{
+				throw new ArgumentException(ex.Message, nameof(restoredProb.MatrixT));
+			}
+
+
+			return new SquareAssignmentProblem(matrixC: matrixC, matrixT: matrixT);
 		}
 
 	}
